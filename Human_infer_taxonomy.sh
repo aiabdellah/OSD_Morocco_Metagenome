38f7a0104f9d7b1a7e513563aa4193aed45213ee
Abdellah IDRISSI AZAMI <36586950+aiabdellah@users.noreply.github.com>
2019-04-08 23:18:57 +0100

    Update Human_infer_taxonomy.sh

--- Human_infer_taxonomy.sh
+++ Human_infer_taxonomy.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/24/humann2/contigs_osd24/contigs_genefamilies.tsv --level Genus -r uniref50 -o ~/OSD_Project/Metagenome/24/humann2/contigs_osd24/classg.tsv
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/24/humann2/contigs_osd24/contigs_genefamilies.tsv -r uniref50 -o ~/OSD_Project/Metagenome/24/humann2/contigs_osd24/classf.tsv
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/91/humann2/contigs_osd91/contigs_genefamilies.tsv --level Genus -r uniref50 -o ~/OSD_Project/Metagenome/91/humann2/contigs_osd91/classg.tsv
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/91/humann2/contigs_osd91/contigs_genefamilies.tsv -r uniref50 -o ~/OSD_Project/Metagenome/91/humann2/contigs_osd91/classf.tsv
-
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/47/humann2/contigs_osd47/contigs_genefamilies.tsv --level Genus -r uniref50 -o ~/OSD_Project/Metagenome/47/humann2/contigs_osd47/classg.tsv
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/47/humann2/contigs_osd47/contigs_genefamilies.tsv -r uniref50 -o ~/OSD_Project/Metagenome/47/humann2/contigs_osd47/classf.tsv
-
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/81/humann2/contigs_osd81/contigs_genefamilies.tsv --level Genus -r uniref50 -o ~/OSD_Project/Metagenome/81/humann2/contigs_osd81/classg.tsv
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/81/humann2/contigs_osd81/contigs_genefamilies.tsv -r uniref50 -o ~/OSD_Project/Metagenome/81/humann2/contigs_osd81/classf.tsv
-
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/90/humann2/contigs_osd90/contigs_genefamilies.tsv --level Genus -r uniref50 -o ~/OSD_Project/Metagenome/90/humann2/contigs_osd90/classg.tsv
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/90/humann2/contigs_osd90/contigs_genefamilies.tsv -r uniref50 -o ~/OSD_Project/Metagenome/90/humann2/contigs_osd90/classf.tsv
-
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/93/humann2/contigs_osd93/contigs_genefamilies.tsv --level Genus -r uniref50 -o ~/OSD_Project/Metagenome/93/humann2/contigs_osd93/classg.tsv
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/93/humann2/contigs_osd93/contigs_genefamilies.tsv -r uniref50 -o ~/OSD_Project/Metagenome/93/humann2/contigs_osd93/classf.tsv
-
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/94/humann2/contigs_osd94/contigs_genefamilies.tsv --level Genus -r uniref50 -o ~/OSD_Project/Metagenome/94/humann2/contigs_osd94/classg.tsv
-humann2_infer_taxonomy -i ~/OSD_Project/Metagenome/94/humann2/contigs_osd94/contigs_genefamilies.tsv -r uniref50 -o ~/OSD_Project/Metagenome/94/humann2/contigs_osd94/classf.tsv
-
+humann2_infer_taxonomy -i ~/Metagenome/humann2/contigs/contigs_genefamilies.tsv --level Genus -r uniref50 -o ~/Metagenome/humann2/contigs/classg.tsv
+humann2_infer_taxonomy -i ~/Metagenome/humann2/contigs/contigs_genefamilies.tsv -r uniref50 -o ~/Metagenome/humann2/contigs/classf.tsv
