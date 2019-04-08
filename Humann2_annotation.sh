d4b349e517717788e4d0d551fca75550683c78fa
Abdellah IDRISSI AZAMI <36586950+aiabdellah@users.noreply.github.com>
2019-04-08 23:19:46 +0100

    Update Humann2_annotation.sh

--- Humann2_annotation.sh
+++ Humann2_annotation.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-humann2 -i ~/OSD_Project/Metagenome/Assembled/24/contigs.fasta --output-format tsv -o ~/OSD_Project/Metagenome/24/humann2/contigs_osd24
-humann2 -i ~/OSD_Project/Metagenome/Assembled/91/contigs.fasta --output-format tsv -o ~/OSD_Project/Metagenome/91/humann2/contigs_osd91 
-humann2 -i ~/OSD_Project/Metagenome/Assembled/47/contigs.fasta --output-format tsv -o ~/OSD_Project/Metagenome/47/humann2/contigs_osd47 
-humann2 -i ~/OSD_Project/Metagenome/Assembled/81/contigs.fasta --output-format tsv -o ~/OSD_Project/Metagenome/81/humann2/contigs_osd81
-humann2 -i ~/OSD_Project/Metagenome/Assembled/90/contigs.fasta --output-format tsv -o ~/OSD_Project/Metagenome/90/humann2/contigs_osd90
-humann2 -i ~/OSD_Project/Metagenome/Assembled/93/contigs.fasta --output-format tsv -o ~/OSD_Project/Metagenome/93/humann2/contigs_osd93
-humann2 -i ~/OSD_Project/Metagenome/Assembled/94/contigs.fasta --output-format tsv -o ~/OSD_Project/Metagenome/94/humann2/contigs_osd94
-
+humann2 -i ~/Metagenome/Assembled/contigs.fasta --output-format tsv -o ~/Metagenome/humann2/contigs
