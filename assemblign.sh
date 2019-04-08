8a414a49efb1aa4b84f8ffb17f0089dbafbf8f15
Abdellah IDRISSI AZAMI <36586950+aiabdellah@users.noreply.github.com>
2019-04-08 23:20:51 +0100

    Update assemblign.sh

--- assemblign.sh
+++ assemblign.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-metaspades.py -1 ~/OSD_Project/Metagenome/Trimmed/OSD24_TP1_shotgun_raw.fastq.gz -2 ~/OSD_Project/Metagenome/Trimmed/OSD24_TP2_shotgun_raw.fastq.gz -o OSD_Project/Metagenome/Assembled/24
-metaspades.py -1 ~/OSD_Project/Metagenome/Trimmed/OSD91_TP1_shotgun_raw.fastq.gz -2 ~/OSD_Project/Metagenome/Trimmed/OSD91_TP2_shotgun_raw.fastq.gz -o OSD_Project/Metagenome/Assembled/91
-metaspades.py -1 ~/OSD_Project/Metagenome/Trimmed/OSD47_TP1_shotgun_raw.fastq.gz -2 ~/OSD_Project/Metagenome/Trimmed/OSD47_TP2_shotgun_raw.fastq.gz -o OSD_Project/Metagenome/Assembled/47
-metaspades.py -1 ~/OSD_Project/Metagenome/Trimmed/OSD90_TP1_shotgun_raw.fastq.gz -2 ~/OSD_Project/Metagenome/Trimmed/OSD90_TP2_shotgun_raw.fastq.gz -o OSD_Project/Metagenome/Assembled/90
-metaspades.py -1 ~/OSD_Project/Metagenome/Trimmed/OSD81_TP1_shotgun_raw.fastq.gz -2 ~/OSD_Project/Metagenome/Trimmed/OSD81_TP2_shotgun_raw.fastq.gz -o OSD_Project/Metagenome/Assembled/81
-metaspades.py -1 ~/OSD_Project/Metagenome/Trimmed/OSD93_TP1_shotgun_raw.fastq.gz -2 ~/OSD_Project/Metagenome/Trimmed/OSD93_TP2_shotgun_raw.fastq.gz -o OSD_Project/Metagenome/Assembled/93
-metaspades.py -1 ~/OSD_Project/Metagenome/Trimmed/OSD94_TP1_shotgun_raw.fastq.gz -2 ~/OSD_Project/Metagenome/Trimmed/OSD94_TP2_shotgun_raw.fastq.gz -o OSD_Project/Metagenome/Assembled/94
+metaspades.py -1 ~/Metagenome/Trimmed/TP1_shotgun.fastq.gz -2 ~/Metagenome/Trimmed/TP2_shotgun.fastq.gz -o ~/Metagenome/Assembled
 
